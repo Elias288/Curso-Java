4f18b4e2d081480fdc5fabc40a91c7bd48fc3a69
AgustinPerez905 <agustinperez2527@gmail.com>
2025-09-15 21:25:36 -0300

    ejercicio_hilos3(no terminado)

--- src/Practica/Ejercicio_Hilos3/src/Buffer.java
+++ src/Practica/Ejercicio_Hilos3/src/Buffer.java
@@ -2,42 +2,42 @@ public class Buffer {
     public int valor = 0;
     public boolean disponibilidad = false;
 
-    public synchronized void producir (int valor) {
+    public synchronized void producir(int valor) {
         try {
-            if (this.valor != 0) {
+            while (this.valor != 0) {
                 System.out.println("productor espera");
                 wait();
 
-            }else {
-                disponibilidad = true;
-                this.valor = valor;
-                System.out.println("producir " + valor);
-                notify();
             }
+            disponibilidad = true;
+            this.valor = valor;
+            System.out.println("producir " + valor);
+            notify();
+
         } catch (InterruptedException e) {
-             throw new RuntimeException(e);
+            throw new RuntimeException(e);
         }
-}
+    }
 
-    public synchronized void consumir () {
+    public synchronized void consumir() {
 
-            try {
-                if (this.valor == 0) {
+        try {
+            while (this.valor == 0) {
                 System.out.println("consumidor espera");
 
                 wait();
 
-        } else {
-                    System.out.println("consumir " + valor);
+            }
+            System.out.println("consumir " + valor);
             this.valor = 0;
             disponibilidad = false;
 
             notify();
-        }
 
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
